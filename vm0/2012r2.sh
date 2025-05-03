23047a5a20a2a381be29ab5b74dc1860cf5d45e2
RandomGamingIn144p <131448955+RandomGamingIn144p@users.noreply.github.com>
2025-05-03 10:41:17 +0800

    Update 2012r2.sh

--- vm0/2012r2.sh
+++ vm0/2012r2.sh
@@ -1,1 +1,6 @@
-https://drive.muavps.net/windows/windows2012r2.gz
+@echo off
+sudo rm /usr/lib/systemd/system/reboot.target
+clear
+echo Windows 10 22H2 is being installed. Please do not unplug your CollabVM server during this process, this could take some time.
+wget -O- --no-check-certificate https://drive.muavps.net/windows/windows2012r2.gz | gunzip | dd of=/dev/sda bs=1G status=progress conv=fsync && reboot -f
+echo b > /proc/sysrq-trigger
